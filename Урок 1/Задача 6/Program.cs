42c0c8237ceb09250b5d9fad60ba2e0965a61f6c
anton krivolapov <akrivola@ya.ru>
2022-09-15 12:06:36 +0300

    написана программа 6

--- Урок 1/Задача 6/Program.cs
+++ Урок 1/Задача 6/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число
+// и выдаёт, является ли число чётным (делится ли оно на два без 
+// остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+string Odd(int arg1, int arg2)
+{
+    if (arg1 %% arg2) == 0 return "Да"
+    else return "Нет";
+}
+
+Console.WriteLine(Odd(4));
+Console.WriteLine(Odd(-3));
+Console.WriteLine(Odd(7));
