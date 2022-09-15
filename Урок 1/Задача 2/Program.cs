a7405b819254134fdfaf1c25f2afeb6f53fdaa26
anton krivolapov <akrivola@ya.ru>
2022-09-15 11:40:19 +0300

    написана программа 4, есть ошибки

--- Урок 1/Задача 2/Program.cs
+++ Урок 1/Задача 2/Program.cs
@@ -4,6 +4,7 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
 Console.Write("Введите число # 1: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число # 2: ");
---
+++ Урок 1/Задача 4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int Max(int arg1, int arg2, int arg2)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 2;
+int b1 = 3;
+int c1 = 7;
+int a2 = 44;
+int b2 = 5;
+int c2 = 78;
+int a3 = 22;
+int b3 = 3;
+int c3 = 9;
+
+Console.WriteLine(Max(a1, b1, c1));
+Console.WriteLine(Max(a2, b2, c2));
+Console.WriteLine(Max(a3, b3, c3));
