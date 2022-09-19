645196ec6879c0425c550337d2fb43a7261a5aa3
anton krivolapov <akrivola@ya.ru>
2022-09-19 11:27:52 +0300

    выполнена Задача 10

---
+++ Урок 2/Задача 10/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает
+на вход трёхзначное число и на выходе показывает
+вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+char sMiddle(int intArg1)
+{
+    return intArg1.ToString()[1];
+}
+Console.WriteLine(sMiddle(456));
+Console.WriteLine(sMiddle(782));
+Console.WriteLine(sMiddle(918));
