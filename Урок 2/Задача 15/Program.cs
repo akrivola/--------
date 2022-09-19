f271b3dbf73fa361680ca93a14db8e98920f92e4
anton krivolapov <akrivola@ya.ru>
2022-09-19 12:03:18 +0300

    Задача 15 решена (без массивов)

---
+++ Урок 2/Задача 15/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет, является ли этот день
+выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+string sWeekend(int iArg1)
+{
+    if (iArg1 == 6 | iArg1 == 7)
+    {
+        return "да";
+    }
+    else
+    {
+        return "нет";
+    }
+}
+Console.WriteLine(sWeekend(6));
+Console.WriteLine(sWeekend(7));
+Console.WriteLine(sWeekend(1));
