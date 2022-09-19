eb0d83175daf92e408111256bda4a4e670eb1772
anton krivolapov <akrivola@ya.ru>
2022-09-19 11:52:00 +0300

    Выполнена задача 13

---
+++ Урок 2/Задача 13/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+char sThird(int intArg1)
+{
+    if (intArg1 < 100)
+    {
+        Console.Write("третьей цифры нет");
+        return '\0';
+    }
+    return intArg1.ToString()[2];
+}
+Console.WriteLine(sThird(645));
+Console.WriteLine(sThird(78));
+Console.WriteLine(sThird(32679));
