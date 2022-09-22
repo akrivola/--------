5eae4734efa55565f7b06e11af16cb1bc26aa866
anton krivolapov <akrivola@ya.ru>
2022-09-22 09:59:30 +0300

    задача 19 решена

--- Урок 2/Задача 19/Program.cs
+++ Урок 2/Задача 19/Program.cs
@@ -4,4 +4,20 @@
 14212 -> нет
 12821 -> да
 23432 -> да
+*/
 
+string IfPalindrome(int Arg)
+{
+    string sArg = Arg.ToString();
+    for (int i = 0; i < sArg.Length / 2; i++) // можно проверять только до половины, чтобы ускорить цикл, 
+    {                                         // а середина всегда равна самой себе в нечетных кол-вах цифр 
+        if (sArg[i] != sArg[sArg.Length - i - 1] )
+        {
+            return "нет";
+        }
+    }
+    return "да";
+}
+Console.WriteLine(IfPalindrome(14212));
+Console.WriteLine(IfPalindrome(12821));
+Console.WriteLine(IfPalindrome(23432));
