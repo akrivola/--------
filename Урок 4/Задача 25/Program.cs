70aeed6dd26b91d2af00be7bc5c696a61bede204
anton krivolapov <akrivola@ya.ru>
2022-09-26 14:49:56 +0300

    выполнена задача 25

--- Урок 4/Задача 25/Program.cs
+++ Урок 4/Задача 25/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два
+числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+int power(int arg1, int arg2)
+{
+    int result = arg1;
+    for (int i = 2; i <= arg2; i++)
+    {
+        result = result * arg1;
+    }
+    return result;
+}
+Console.WriteLine(power(3, 5));
+Console.WriteLine(power(2, 4));
