cb183d69dfe33857adcd16238b20131a261c9e17
anton krivolapov <akrivola@ya.ru>
2022-09-26 15:08:22 +0300

    Решена задача 27

--- Урок 4/Задача 27/Program.cs
+++ Урок 4/Задача 27/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+int sum(int Arg)
+{
+    int result = 0;
+    while(Arg > 0)
+    {
+        result = result + (Arg % 10);
+        Arg = Arg / 10;
+    }
+    // for (int count = 1; count <= NumberOfDigits; count++)
+    // {
+    //     result = result + Console.WriteLine("третьей цифры нет");
+    // }
+    return result;
+}
+Console.WriteLine(sum(452));
+Console.WriteLine(sum(82));
+Console.WriteLine(sum(9012));
