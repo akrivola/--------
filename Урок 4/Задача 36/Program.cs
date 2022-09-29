b7688bdbd2ddf8a4a9ffedd2dc4d59811ccc627a
anton krivolapov <akrivola@ya.ru>
2022-09-29 09:37:36 +0300

    Задача 36 решена

--- Урок 4/Задача 36/Program.cs
+++ Урок 4/Задача 36/Program.cs
@@ -5,3 +5,45 @@
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
 */
+int[] populateRandom(int length)
+{
+    int[] result = new int[length];
+    for (int i=0; i < length; i++)
+    {
+        result[i] = new Random().Next(-99, 100);
+    }
+    return result;
+}
+
+string outArray(int[] array)
+{
+    string result = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += array[i];
+        if (i != array.Length - 1) 
+        {
+            result += ", ";
+        }
+    }
+    result += "]";
+    return result;
+}
+
+int sumOddPlaces(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            result += array[i];
+        }
+    }
+    return result;
+}
+
+Console.Write("Введите длину массива: ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+int[] array = populateRandom(arrayLength);
+Console.WriteLine($"Количество четных чисел в массиве {outArray(array)} -> {sumOddPlaces(array)}");
