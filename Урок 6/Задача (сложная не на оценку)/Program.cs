b5acdfe705676e72d3981d7bbdd3fc29726b2633
anton krivolapov <akrivola@ya.ru>
2022-10-04 11:40:53 +0300

    Задача 43 решена

--- Урок 6/Задача (сложная не на оценку)/Program.cs
+++ Урок 6/Задача (сложная не на оценку)/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+*Задача(сложная не на оценку) *
+Напишите программу, которая находит подмножество данного 
+множества чисел такое, что сумма его элементов равна заданному числу
+*/
+int[] populateRandom(int length)
+{
+    int[] result = new int[length];
+    for (int i=0; i < length; i++)
+    {
+        result[i] = new Random().Next(-9, 10);
+    }
+    return result;
+}
+
+string outArray(int[] array)
+{
+    string result = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += array[i];
+        if (i != array.Length - 1) 
+        {
+            result += ", ";
+        }
+    }
+    result += "]";
+    return result;
+}
+
+
+// заполняем массив размером 1..10 числами в диапазоне -9..9
+Random rnd = new Random();
+int size = rnd.Next(1, 11);
+int[] array = populateRandom(size);
+Console.WriteLine($"{outArray(array)}");
--- Урок 6/Задача 43/Program.cs
+++ Урок 6/Задача 43/Program.cs
@@ -6,3 +6,15 @@ y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
+double k1, b1, k2, b2, x, y;
+Console.Write("Введите b1:");
+b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1:");
+k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2:");
+b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2:");
+k2 = Convert.ToDouble(Console.ReadLine());
+x = (b2 - b1) / (k1 - k2);
+y = k1 * x + b1;
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
