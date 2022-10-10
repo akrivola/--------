b06133ab394b6399ed0076dfedb09dab41987ba0
anton krivolapov <akrivola@ya.ru>
2022-10-10 13:11:29 +0300

    выполнена задача 50

--- Урок 7/Задача 50/Program.cs
+++ Урок 7/Задача 50/Program.cs
@@ -19,3 +19,36 @@
 [8][9][10][11]
 8 4 2 4
 */
+
+void posInArray(int[,] array, int position)
+{
+    Console.Write($"{position} -> ");
+    // сразу проверяем, если позиция > числа элементов
+    if (array.GetLength(0) * array.GetLength(1) < position + 1)
+    {
+        Console.WriteLine("такого числа в массиве нет");
+        return;
+    }
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (count == position)
+            {
+                Console.WriteLine(array[i, j]);
+                return;
+            }
+            count++;
+        }
+    }
+}
+
+int[,] array = 
+{
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4}
+};
+posInArray(array, 17);
+posInArray(array, 5);
--- Урок 7/Задача 52/Program.cs
+++ Урок 7/Задача 52/Program.cs
@@ -8,3 +8,26 @@
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
+
+double average(int[,] array)
+{
+    
+}
+
+void printArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console            
+        }
+    }
+}
+
+int[,] array = 
+{
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4}
+};
