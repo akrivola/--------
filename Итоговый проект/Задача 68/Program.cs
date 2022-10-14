6e7d06e221b1b2a8f3503573be4fa8e008ab505e
anton krivolapov <akrivola@ya.ru>
2022-10-14 10:57:52 +0300

    сделана 68

--- Итоговый проект/Задача 68/Program.cs
+++ Итоговый проект/Задача 68/Program.cs
@@ -11,7 +11,9 @@ int Ackermann(int m, int n)
         return n + 1;
     else
         if((m > 0) && (n == 0))
-            
+            return Ackermann(m - 1, 1);
+        else
+            return Ackermann(m - 1, Ackermann(m, n - 1));
 }
 
 Console.Write("M = ");
