a4e0634caacd6926f69f9b71b2c92dd503d78937
anton krivolapov <akrivola@ya.ru>
2022-10-14 10:30:43 +0300

    сделана 66

--- Итоговый проект/Задача 66/Program.cs
+++ Итоговый проект/Задача 66/Program.cs
@@ -5,3 +5,20 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+
+int sumNatural(int M, int N)
+{
+    int result = M;
+    if(M == N)
+    {
+        return result;
+    }    
+    result += sumNatural(M + 1, N);
+    return result;
+}
+
+Console.Write("M = ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"M = {M}; N = {N} -> - > {sumNatural(M, N)}");
